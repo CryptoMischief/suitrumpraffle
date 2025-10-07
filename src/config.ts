3f921dd7b407400437ff08bd9a500e1e25df09a8
CryptoMischief <124801055+CryptoMischief@users.noreply.github.com>
2025-10-07 15:35:30 +0800

    Update config.ts
    updated cetus pool

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,8 @@ export const DEFAULT_TOKEN_DECIMALS = 6;
 
 export const TOKEN_NAME = "SUI TRUMP"
 // Define the Move event type for swaps (e.g., Cetus, Kriya, or other DEX)
-export const MOVE_EVENT_TYPE_CETUS = "0xeffc8ae61f439bb34c9b905ff8f29ec56873dcedf81c7123ff2f1f67c45ec302::cetus::CetusSwapEvent";
+export const MOVE_EVENT_TYPE_CETUS_POOL = "0x1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb::pool::SwapEvent"
+export const MOVE_EVENT_TYPE_CETUS_ROUTER = "0x33ec64e9bb369bf045ddc198c81adbf2acab424da37465d95296ee02045d2b17::router::SwapEvent"
 export const MOVE_EVENT_TYPE_SETTLE = "0xe8f996ea6ff38c557c253d3b93cfe2ebf393816487266786371aa4532a9229f2::settle::Swap";
 export const MOVE_EVENT_TYPE_BLUEMOVE = "0x764b8132a94d35abc9dfd91b23a0757b2a717d5ecb04c03098794aa2a508db91::bluemove::BlueMoveSwapEvent";
 export const MOVE_EVENT_TYPE_FLOWX = "0xc263060d3cbb4155057f0010f92f63ca56d5121c298d01f7a33607342ec299b0::universal_router::Swap";
